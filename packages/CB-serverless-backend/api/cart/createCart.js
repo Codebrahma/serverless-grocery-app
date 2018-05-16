9b9227028e2b33e97d398ca335d6c104c75d9bed
prasanna1211 <prasanna@codebrahma.com>
2018-05-16 10:11:47 +0530

    Added create and get cart functions

---
+++ packages/CB-serverless-backend/api/cart/createCart.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import AWS from 'aws-sdk';
+import _ from 'lodash';
+import filter from 'lodash/filter';
+import uniqBy from 'lodash/uniqBy';
+import map from 'lodash/map';
+import slice from 'lodash/slice';
+
+import awsConfigUpdate from '../../utils/awsConfigUpdate';
+import getErrorResponse from '../../utils/getErrorResponse';
+import getSuccessResponse from '../../utils/getSuccessResponse';
+
+awsConfigUpdate();
+
+export const main = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+  
+  const {
+    userId,
+    cartData,
+  } = JSON.parse(event.body);
+  console.log(userId, cartData, typeof cartData);
+  var params = {
+    TableName: 'cart',
+    Key: {
+      userId: parseInt(userId),
+    },
+    ExpressionAttributeNames: {
+      '#cartData': 'cartData'
+    },
+    ExpressionAttributeValues: {
+      ':cartData': JSON.stringify(CharacterData),
+    },
+    UpdateExpression: 'SET #cartData = :cartData',
+  };
+
+    const queryPromise = documentClient.update(params).promise();
+
+    queryPromise
+      .then((data) => {
+        callback(null, getSuccessResponse(data))
+      })
+      .catch((error) => {
+        console.log(error.message);
+        getErrorResponse(callback, 500, JSON.stringify(error.message))
+      });
+}
+
+
+
---
+++ packages/CB-serverless-backend/api/cart/getCart.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import AWS from 'aws-sdk';
+import _ from 'lodash';
+import filter from 'lodash/filter';
+import uniqBy from 'lodash/uniqBy';
+import map from 'lodash/map';
+import slice from 'lodash/slice';
+
+import awsConfigUpdate from '../../utils/awsConfigUpdate';
+import getErrorResponse from '../../utils/getErrorResponse';
+import getSuccessResponse from '../../utils/getSuccessResponse';
+
+awsConfigUpdate();
+
+export const main = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+  
+  if (!event.queryStringParameters) {
+    getErrorResponse(callback, 400, 'userId is not present in params')
+  }
+  
+  var params = {
+    TableName: 'cart',
+    Key: {
+      userId: parseInt(event.queryStringParameters.userId),
+    },
+  };
+
+    const queryPromise = documentClient.get(params).promise();
+
+    queryPromise
+      .then((data) => {
+        callback(null, getSuccessResponse(data))
+      })
+      .catch((error) => {
+        console.log(error.message);
+        getErrorResponse(callback, 500, JSON.stringify(error.message))
+      });
+}
+
+
+
