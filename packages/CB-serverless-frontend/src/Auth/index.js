34eed774db5c3a607c825411e56a1134fab66b80
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-17 11:49:21 +0530

    Login Form style tweaks
    show error message on login/signup failed in form

--- packages/CB-serverless-frontend/src/Auth/index.js
+++ packages/CB-serverless-frontend/src/Auth/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import styled from 'styled-components';
 
 import LoginForm from './LoginForm';
 import { connect } from 'react-redux';
@@ -8,6 +9,44 @@ import { bindActionCreators } from 'redux';
 import { attemptLogin } from './actionCreators';
 
 import styles from './styles.css';
+
+const LoginContainer = styled.div`
+    background: #fff;
+    margin: 2em auto;
+    padding: 0px 0px 50px;
+    box-shadow: 0px 0px 25px 4px #ddd;
+    height: auto;
+    min-height: 400px;
+    vertical-align: middle;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    transform: translate(0%, 50%);
+    
+    @media (min-width: 1280px) {
+        width: 40%;
+    }
+    
+    @media (max-width: 1279px){    
+        width: 60%;
+    }
+    
+    @media (min-width: 601px) and (max-width: 800px){    
+        width: 100%;
+    }
+    
+    @media (max-width: 600px){    
+        width: 80%;
+        transform: translateY(0);
+    }
+    
+    @media (max-width: 480px){    
+        width: 100%;
+        margin: 0;
+        transform: translateY(0);
+    }
+    
+`;
 
 class Login extends React.Component {
   constructor(props) {
@@ -37,7 +76,7 @@ class Login extends React.Component {
     }
     return (
       <React.Fragment>
-        <div className="tab-container">
+        <LoginContainer>
           <Tabs
             value={this.state.authScreen}
             onChange={this.handleChange}
@@ -45,13 +84,17 @@ class Login extends React.Component {
             <Tab label="Register" value="register" />
             <Tab label="Login" value="login" />
           </Tabs>
+          {
+            this.props.errorMessage &&
+            <div>{this.props.errorMessage}</div>
+          }
           <LoginForm
             type={this.state.authScreen}
             shouldDisableLogin={shouldDisableLogin}
             requireVerification={requireVerification}
             onSubmit={() => { this.props.attemptLogin(this.state.authScreen, requireVerification) }}
           />
-        </div>
+        </LoginContainer>
       </React.Fragment>
     );
   }
