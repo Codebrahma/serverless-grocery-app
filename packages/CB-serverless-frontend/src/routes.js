2f66a09a168bfd23142e70de8b7b97a968591be3
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-17 13:04:56 +0530

    Add route for category items page

--- packages/CB-serverless-frontend/src/routes.js
+++ packages/CB-serverless-frontend/src/routes.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable react/prop-types,no-unused-vars,no-unused-vars */
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,29 +7,31 @@ import {
 import { Auth } from 'aws-amplify';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Header from './components/header';
 
 import AuthModule from './Auth';
+import Header from './components/header';
 import ProductHome from './components/ProductHome';
 import { updateAuth } from './Auth/actionCreators';
+import CategoryItems from './components/categoryItems';
 
-const DefaultLayout = ({component: Component, ...rest}) => {
-  return (
-    <Route {...rest} render={matchProps => (
+const DefaultLayout = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={matchProps => (
       <div>
         <Header />
         <Component {...matchProps} />
       </div>
-    )} />
-  )
-};
+    )}
+  />
+);
 
 class Routes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginReady: false
-    }
+      loginReady: false,
+    };
     // Remove when real signout component is ready
     this.handleLogout = this.handleLogout.bind(this);
   }
@@ -42,12 +45,18 @@ class Routes extends React.Component {
           isAuthenticating: false,
           isAuthenticated: true,
           identityId: data,
-        })
+        });
       }).catch((error) => {
         this.finishAuthentication();
       });
     } catch (e) {
       // to do
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.state.loginReady && this.props.isAuthenticating && !nextProps.isAuthenticating) {
+      this.setState({ loginReady: true });
     }
   }
 
@@ -57,7 +66,7 @@ class Routes extends React.Component {
       isAuthenticated: false,
       identityId: null,
     });
-  }
+  };
 
   resetAndStartAuthentication = () => {
     this.props.updateAuth({
@@ -65,19 +74,13 @@ class Routes extends React.Component {
       isAuthenticated: false,
       identityId: null,
     });
-  }
+  };
 
   finishAuthentication = () => {
     this.props.updateAuth({
-      isAuthenticating: false
+      isAuthenticating: false,
     });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!this.state.loginReady && this.props.isAuthenticating && !nextProps.isAuthenticating) {
-      this.setState({loginReady: true});
-    }
-  }
+  };
 
   // Remove when real signout component is ready
   async handleLogout() {
@@ -85,29 +88,27 @@ class Routes extends React.Component {
     this.resetInitialState();
   }
 
-  // Remove when real component is ready
-  Path1 = () => <div><h3>Path1</h3><button onClick={this.handleLogout}>Signout</button></div>
-  Path2 = () => <div><h3>Path2</h3><button onClick={this.handleLogout}>Signout</button></div>
-
   render() {
     return (
       <Router>
         <div className="root-container">
-        {
-          !this.props.isAuthenticated && this.state.loginReady?
-          <Route render={() => <AuthModule />} />
-          :
-          <React.Fragment>
-            <DefaultLayout exact path='/' component={ProductHome} />
-          </React.Fragment>
-        }
+          {
+            !this.props.isAuthenticated && this.state.loginReady ?
+              <Route render={() => <AuthModule />} />
+              :
+              <React.Fragment>
+                <DefaultLayout exact path="/" component={ProductHome} />
+                <DefaultLayout exact path="/category/:category" component={CategoryItems} />
+                <DefaultLayout exact path="/category/" component={CategoryItems} />
+              </React.Fragment>
+          }
         </div>
       </Router>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
   isAuthenticating: state.auth.isAuthenticating,
   identityId: state.auth.identityId,
