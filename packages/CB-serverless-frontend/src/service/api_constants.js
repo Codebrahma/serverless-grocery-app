496fbabc500c2abc47e909e4f91d7cc796cb13c2
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-22 13:15:49 +0530

    Methods to get Token from storage

--- packages/CB-serverless-frontend/src/service/api_constants.js
+++ packages/CB-serverless-frontend/src/service/api_constants.js
@@ -1,5 +1,7 @@
+import { isEmpty, isNil } from 'lodash';
+
 const LOCAL_API_BASE = 'http://localhost:3000';
-const API_BASE = 'https://ki9zw0fyi2.execute-api.ap-south-1.amazonaws.com/dev/';
+const API_BASE = 'https://ki9zw0fyi2.execute-api.ap-south-1.amazonaws.com/dev';
 
 export const GROCERY_INFO_URL = `${API_BASE}/grocery`;
 export const GROCERIES_URL = `${API_BASE}/groceries`;
@@ -7,3 +9,32 @@ export const CATEGORY_URL = `${GROCERIES_URL}?category=`;
 export const CART_URL = `${API_BASE}/cart`;
 export const PAYMENT_URL = `${API_BASE}/pay`;
 export const ORDER_URL = `${API_BASE}/createOrder`;
+
+
+export const getToken = () => {
+  const session = localStorage.getItem('userSession');
+  if (!isNil(session) && !isEmpty(session) && !isNil(session)) {
+    return JSON.parse(session).signInUserSession;
+  }
+  return {};
+};
+
+export const getAccessToken = () => {
+  const obj = getToken();
+  if (!isNil(obj) && !isNil(obj.accessToken)) { return obj.accessToken.jwtToken; }
+  return null;
+};
+
+
+export const getIdToken = () => {
+  const obj = getToken();
+  if (!isNil(obj) && !isNil(obj.idToken)) { return obj.idToken.jwtToken; }
+  return null;
+};
+
+
+export const authHeaders = () => ({
+  headers: {
+    Authorization: getIdToken(),
+  },
+});
