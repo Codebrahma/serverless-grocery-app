17e05f3e1b0fcd7432d4acd1cc6cf7b6195a962f
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-25 12:05:14 +0530

    CategoryItems
    SideBar styling

--- packages/CB-serverless-frontend/src/components/Category/sub-categories.js
+++ packages/CB-serverless-frontend/src/components/Category/sub-categories.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 import Checkbox from 'material-ui/Checkbox';
 
 const Container = styled.div`
-  width: 15%;
-  margin-top: 5%;
+  width: 25%;
+  margin: 2em 1em;
+  background: #fff;
+  padding: 2em 1em;
+  height: 100%;
+  box-shadow: 0px 0px 10px 0px #ddd;
 `;
 
 export default ({ subCategories, checked, onCheck }) => (
@@ -13,12 +17,19 @@ export default ({ subCategories, checked, onCheck }) => (
     subCategories.map((value, index) => {
       const label = value.charAt(0).toUpperCase() + value.slice(1);
       return (
-        <div key={index}>
+        <div
+          style={{
+            padding: '1em 0em',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+          key={index}
+        >
           <Checkbox
             label={label}
             checked={checked[value]}
             onCheck={() => (onCheck(value))}
-            style={{ marginBottom: 16 }}
+            style={{ marginBottom: 0 }}
             labelStyle={{ textAlign: 'left', marginLeft: '5%' }}
           />
         </div>
