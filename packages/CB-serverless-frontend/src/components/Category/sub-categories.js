d77bfd0ca718da1e789ea3221d8a6ba149b011e1
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-25 12:21:21 +0530

    Category Sub Categories skeleton loading

--- packages/CB-serverless-frontend/src/components/Category/sub-categories.js
+++ packages/CB-serverless-frontend/src/components/Category/sub-categories.js
@@ -11,8 +11,28 @@ const Container = styled.div`
   box-shadow: 0px 0px 10px 0px #ddd;
 `;
 
+const SubCategorySkeleton = styled.div`
+  margin: 1em 0em;
+  width: 100%;
+  background: #fff;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(90deg,rgba(243,243,243,0) 0,rgba(243,243,243,0.4) 50%, rgba(243,243,243,0) 100%), linear-gradient(#f3f3f3 30px,transparent 0), linear-gradient(#f3f3f3 31px,transparent 0);
+  background-size: 100px 100%, 30px 50px, 50% 20px;
+  background-position: -150% 0, 5px 5px, 40% 10px;
+  height: 40px;
+  animation: loadingSubCat 2s infinite;  
+`;
+
 export default ({ subCategories, checked, onCheck }) => (
   <Container>
+    {
+      subCategories.length === 0 &&
+      [1, 2, 3, 4].map(val => (
+        <SubCategorySkeleton>
+            &nbsp;
+        </SubCategorySkeleton>
+        ))
+    }
     {
     subCategories.map((value, index) => {
       const label = value.charAt(0).toUpperCase() + value.slice(1);
