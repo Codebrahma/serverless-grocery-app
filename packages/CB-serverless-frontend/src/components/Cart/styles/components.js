339b25319f484e4d49ae675b3c3a70fe06720d35
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-30 21:17:09 +0530

    Refractor cart module
    Move styled components to new file

---
+++ packages/CB-serverless-frontend/src/components/Cart/styles/components.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import { Wrapper } from '../../../base_components';
+
+export const CartWrapper = Wrapper.extend`
+  color: #222;
+  background: #f5f5f5;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+`;
+
+export const CartMain = styled.div`
+  flex: 7;
+  padding: 3em 2em;
+  background: #fff;
+  box-shadow: 0 0 10px 1px #eee;
+`;
+
+export const EmptyCart = styled.div`
+  padding: 2em;
+  font-size: 20px;
+  text-align: center;
+  color: #888;
+  background: #eee;
+  margin: 4em auto 1em;
+`;
+
+export const CartHead = styled.h1`
+  border-bottom: 1px solid #eee;
+  padding-bottom: 1em;
+`;
+
+export const RightSideContent = styled.div`
+  margin: 0 1em;
+  flex: 2.5;
+  color: #333;
+`;
+
+export const OrderPending = styled.section`
+  background: #fff;
+  padding: 1em 2em;
+
+  > h3 {
+    margin: 1em 0 2em;
+  }
+
+  > p {
+    margin: 1em 0 3em;
+    font-weight: bold;
+    color: #888;
+    letter-spacing: 0.5px;
+  }
+`;
+
+export const TotalSection = styled.div`
+  margin: 2em auto;
+  font-size: 1.5em;
+  padding: 0 3em;
+  text-align: right;
+  > span:first-child{
+    margin: 0 2em;
+  }
+
+`;
+
