8a51a51f5693b44e2d893ba0d163dffe70c1010c
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-31 20:17:50 +0530

    Comments for payment Token Submit saga

--- packages/CB-serverless-frontend/src/sagas/paymentTokenIdSubmitSaga.js
+++ packages/CB-serverless-frontend/src/sagas/paymentTokenIdSubmitSaga.js
@@ -1,51 +1,51 @@
-import {
-  takeLatest,
-  put,
-  select,
-  call,
-} from 'redux-saga/effects';
-import { API, Auth } from 'aws-amplify';
-import axios from 'axios';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import PaymentRequests from '../service/payment';
 
-const {submitPaymentRequest} = PaymentRequests;
+const { submitPaymentRequest } = PaymentRequests;
 
 function* submitPayment(action) {
-  const { tokenId, orderId, email, userId } = action.payload;
-  if (!tokenId || !orderId) return ;
+  const {
+    tokenId, orderId, email, userId,
+  } = action.payload;
+  if (!tokenId || !orderId) return;
   const payload = {
     email,
-	  stripeId: tokenId,
-	  orderId,
-    userId
+    stripeId: tokenId,
+    orderId,
+    userId,
   };
   yield put({
-    type: 'PAYMENT_IN_PROGRESS'
-  })
+    type: 'PAYMENT_IN_PROGRESS',
+  });
   try {
-    const response =  yield call(() => submitPaymentRequest(payload));
-    const {data} = response;
+    // make request to process payment with the passed info
+    // server will confirm order only if payment is successful
+    const response = yield call(() => submitPaymentRequest(payload));
+    const { data } = response;
     if (data.success) {
       yield put({
-        type: 'PAYMENT_SUCCESS'
-      })
+        type: 'PAYMENT_SUCCESS',
+      });
       yield put({
         type: 'FETCH_ALL_ORDERS',
       });
-    }
-    else
+    } else {
       yield put({
         type: 'PAYMENT_FAILURE',
-        payload: {error: data.error}
-      })
-  } catch(e) {
+        payload: { error: data.error },
+      });
+    }
+  } catch (e) {
     yield put({
       type: 'PAYMENT_FAILURE',
-      payload: {error: 'Something went wrong, Please try again.'}
-    })
+      payload: { error: 'Something went wrong, Please try again.' },
+    });
   }
 }
 
+/**
+ * Saga to handle payment Token id submit on server
+ */
 function* paymentTokenIdSubmitSaga() {
   yield takeLatest('SUBMIT_PAYMENT_TOKEN_ID', submitPayment);
 }
