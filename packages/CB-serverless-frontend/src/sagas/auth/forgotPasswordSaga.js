8ed10815cbc83802b36c7e76af111303d20b1009
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-31 18:56:14 +0530

    Comments on forgotPasswordSaga

--- packages/CB-serverless-frontend/src/sagas/auth/forgotPasswordSaga.js
+++ packages/CB-serverless-frontend/src/sagas/auth/forgotPasswordSaga.js
@@ -7,23 +7,57 @@ import {
 import { Auth } from 'aws-amplify';
 import loginFailureSaga from './loginFailureSaga';
 
+/**
+ * Get values from forgotPassword form
+ */
 const forgotPasswordFormSelector = state => state.form.forgotPassword.values;
 
 function* forgotPassword(action) {
+  /**
+   * Promise returned by the 'signIn' method of Amplify
+   * @param username {string} email of user
+   * @param password {string} password
+   * @returns {Promise<any>}
+   */
   const loginPromise = ({ username, password }) => Auth.signIn(username, password);
-  const forgotPasswordPromise = ({username, code, password}) => Auth.forgotPasswordSubmit(username, code, password);
+  /**
+   * Promise returned by Amplify library for forgotPassword
+   * @param username {string} email of user
+   * @param code {number} code sent on email of user, required to reset password
+   * @param password {string} new password
+   */
+  const forgotPasswordPromise = ({
+    username, code, password,
+  }) => Auth.forgotPasswordSubmit(username, code, password);
+
+  /**
+   * Promise to get the authenticated user after the password changes
+   */
   const currentAuthenticatedUserPromise = () => Auth.currentAuthenticatedUser();
+  /**
+   * Promise to get User's info other than email
+   */
   const userDataPromise = () => Auth.currentUserInfo();
   try {
-    yield put({type: 'IN_PROGRESS'});
-    const {username, code, password} = yield select(forgotPasswordFormSelector) ;
+    // send action to notify task in progess
+    yield put({ type: 'IN_PROGRESS' });
 
-    yield call(forgotPasswordPromise, { username, code, password});
+    // Get the values from the forgotPassword form
+    const { username, code, password } = yield select(forgotPasswordFormSelector);
+
+    // Call the promise with the input values of form
+    yield call(forgotPasswordPromise, { username, code, password });
+
+    // Sign the user in, once password change is successful
     yield call(loginPromise, { username, password });
 
+    // Get User credentials eg., all token etc
     const currentCredentials = yield call(currentAuthenticatedUserPromise);
+
+    // Get User other info, eg., name, phone number
     const currentUserData = yield call(userDataPromise);
 
+    // send action to mark login success and save all info
     yield put({
       type: 'ATTEMPT_LOGIN_SUCCESS',
       payload: {
@@ -33,11 +67,15 @@ function* forgotPassword(action) {
     });
   } catch (e) {
     console.log(e);
-    const loginFail = (() => (loginFailureSaga({e, authScreen: 'login' })));
+    // Call the saga in case any error occurs
+    const loginFail = (() => (loginFailureSaga({ e, authScreen: 'login' })));
     yield call(loginFail);
   }
 }
 
+/**
+ * Saga to call on action type 'FORGOT_PASSWORD'
+ */
 function* forgotPasswordSaga() {
   yield takeLatest('FORGOT_PASSWORD', forgotPassword);
 }
