3252e14fe037e5b5734ed5e2a29d3aa1d56a0f1a
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-31 19:38:42 +0530

    Comments in cartItemsAddSaga

--- packages/CB-serverless-frontend/src/sagas/cart/cartItemsAddSaga.js
+++ packages/CB-serverless-frontend/src/sagas/cart/cartItemsAddSaga.js
@@ -2,18 +2,39 @@ import { put, call, select, takeLatest } from 'redux-saga/effects';
 import CartService from '../../service/cart';
 import { deDupeItems } from '../../utils/array';
 
+/**
+ * Get username value from redux store
+ */
 const userIdSelector = state => state.auth.userData && state.auth.userData.username;
+
+/**
+ * Get current cart items from the store
+ */
 const cartItemsSelector = state => state.cart.cartData || [];
+
 const { updateCart } = CartService;
 
+/**
+ * saga called on action
+ */
 function* cartItemsAdd(action) {
   try {
+    // get username from the store
     const userId = yield select(userIdSelector);
-    let currentCart = yield select(cartItemsSelector);
+
+    // get current Cart items from store
+    const currentCart = yield select(cartItemsSelector);
+
+    // Create new empty cart
     let newCart = [];
+    // merge the current cart with the newly added item
     newCart = deDupeItems([...currentCart, ...[action.payload]]);
+
+    // Make API request to update cart at backend for that user
     const response = yield call(() => updateCart(userId, newCart));
     const { resp } = response.data ? response.data : {};
+
+    // trigger the action which will fetch new cart data from backend
     yield put({ type: 'FETCH_CART_ITEMS' });
   } catch (e) {
     console.log(e);
