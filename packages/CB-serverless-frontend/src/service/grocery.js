2e91fcfd96c5038b455405e19f9e351824e4aaba
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-31 20:27:56 +0530

    Comments and unused import remove from 'service/*' files

--- packages/CB-serverless-frontend/src/service/grocery.js
+++ packages/CB-serverless-frontend/src/service/grocery.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
 import { CATEGORY_URL, GROCERIES_URL, GROCERY_INFO_URL } from './api_constants';
 import request from './request';
 
+// Get top 3g groceries item for each category
+// Shown on home page
 export function getTop3Groceries() {
-  return request({url: GROCERIES_URL, method: 'GET'});
+  return request({ url: GROCERIES_URL, method: 'GET' });
 }
 
+// Get info about particular grocery item based on id
 export function getGroceryInfo(groceryId) {
-  return request({url: `${GROCERY_INFO_URL}?id=${groceryId}`, method: 'GET'});
+  return request({ url: `${GROCERY_INFO_URL}?id=${groceryId}`, method: 'GET' });
 }
 
+// Get groceries for a particular category
 export function getCategoryGroceries(category) {
-  return request({url: `${CATEGORY_URL + category}`, method: 'GET'});
+  return request({ url: `${CATEGORY_URL + category}`, method: 'GET' });
 }
