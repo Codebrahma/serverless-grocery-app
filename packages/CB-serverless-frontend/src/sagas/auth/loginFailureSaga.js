9aafced1e8db4fd9e3e8f3b2749dd863050632a8
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-05-31 19:02:59 +0530

    Comments add on loginFailureSaga

--- packages/CB-serverless-frontend/src/sagas/auth/loginFailureSaga.js
+++ packages/CB-serverless-frontend/src/sagas/auth/loginFailureSaga.js
@@ -1,26 +1,39 @@
 import { put } from 'redux-saga/effects';
 import { USER_NOT_VERIFIED, USER_ALREADY_EXIST } from '../../constants/app';
 
-function* loginFailureSaga({e, authScreen}) {
+/**
+ * Saga which handles the error on login/signup
+ */
+function* loginFailureSaga({ e, authScreen }) {
+  // check error message type
   let errorMessage = typeof e === 'string' && e;
-  let userNotVerified = (e.code === 'UserNotConfirmedException')? USER_NOT_VERIFIED : null;
-  switch(e.code) {
+  const userNotVerified = (e.code === 'UserNotConfirmedException') ? USER_NOT_VERIFIED : null;
+
+  // Check and switch based on different errors
+  switch (e.code) {
     case 'UsernameExistsException':
       errorMessage = USER_ALREADY_EXIST;
       break;
     case 'UserNotConfirmedException':
       errorMessage = USER_NOT_VERIFIED;
       break;
+    default:
+      errorMessage = 'Error occured.';
+      break;
   }
+
+  // if error of type then send a 'CLEAN_AUTH' action
   const notAuthorized = (e.code === 'NotAuthorizedException');
   if (notAuthorized) {
-    yield put({type: 'CLEAN_AUTH'});
+    yield put({ type: 'CLEAN_AUTH' });
   }
+
+  // send an action of type which contains errorMessage
   yield put({
     type: 'ATTEMPT_LOGIN_FAILURE',
     payload: {
       type: authScreen,
-      errorMessage: errorMessage || e.message || 'Some error occured',
+      errorMessage: errorMessage || e.message || 'Some error occurred',
     },
   });
 }
